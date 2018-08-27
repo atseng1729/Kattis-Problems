6f4be7e8cd1135fa8031b163350951f5f196ecd4
Arthur Tseng <atseng1729@gmail.com>
2018-08-27 14:45:27 -0400

    added progressive scramble

--- easy_problems/progressive_scramble.cpp
+++ easy_problems/progressive_scramble.cpp
@@ -17,10 +17,15 @@ void encrypt(string s)
 
 void decrypt(string s)
 {
-    int u = 0, length = s.length();
+    int prev = 0, length = s.length();
     FOR(i,0,length) {
-        
+        int temp = (s[i] != ' ') ? s[i] - 'a' + 1 : 0;
+        while (temp < prev) temp += 27;
+        if (temp - prev == 0) cout << ' ';
+        else cout << (char) ((temp - prev + 'a' - 1));
+        prev = temp;
     }
+    cout << '\n';
 }
 
 int main() {
@@ -34,5 +39,6 @@ int main() {
         string temp;
         getline(cin, temp);
         if (temp[0] == 'e') encrypt(temp.substr(2));
+        else decrypt(temp.substr(2));
     }
 }
